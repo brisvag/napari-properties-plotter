32a97c2cd05fff7b9c05e407b47fac7a15b203f5
Lorenzo Gaifas <brisvag@gmail.com>
2021-07-06 16:42:31 +0200

    added example

---
+++ examples/points.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import napari
+
+from napari_properties_plotter import PropertyPlotter
+
+
+points = np.random.rand(100, 2) * 200
+properties = pd.DataFrame({
+    'a': np.random.rand(100) * 20,
+    'b': np.sin(np.linspace(0, 10, 100)),
+    'c': np.random.choice(['red', 'green', 'blue'], 100),
+})
+
+v = napari.Viewer()
+v.add_points(points, properties=properties)
+v.window.add_dock_widget(PropertyPlotter(v), area='bottom')
+
+napari.run()
